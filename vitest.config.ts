eb44b34f013c56264a13827fc5bbb154e38ecf77
bitgumbo <bitgumbo@gmail.com>
2026-04-30 10:40:07 -0400

    feat: vitest config enforces coverage threshold and forwards tsconfig paths rewrites for '@/*'

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-// import path from "node:path";
+import path from 'node:path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
@@ -14,6 +14,18 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['lcov', 'text', 'json', 'html'],
       include: ['src/**/*.ts'],
+
+      thresholds: {
+        lines: 100,
+        statements: 100,
+        branches: 100,
+        functions: 100,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
